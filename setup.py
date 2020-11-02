e5d734860bf9df18eea7c2046ff9e45248b3d8fe
ReneNeu <43846116+ReneNeu@users.noreply.github.com>
2020-11-02 17:19:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="vscode-proxy",
+    version='0.1.0',
+    url="https://github.com/ReneNeu/vscode-proxy",
+    author="Rene Neuhaus",
+    description="Proxy for VS Code to start in Browser",
+    packages=setuptools.find_packages(),
+	  keywords=['Jupyter', 'Proxy', 'VS Code'],
+	  classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'vscode = vscode-proxy:setup_vscode',
+        ]
+    },
+    package_data={
+        'vscode-proxy': ['icons/*'],
+    },
+)
